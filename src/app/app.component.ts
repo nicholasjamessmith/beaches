7a4ee71f22eee9fe8bbd6c9031a19f28a4770791
nicholasjamessmith <njsmith6@gmail.com>
2024-05-09 18:33:36 -0400

    "changed  Hello, World! font color and added HomeComponent + logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component'
 //import { RouterOutlet } from '@angular/router';
 
 //The component below gets rendered to the landing page of the application.
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   //imports: [RouterOutlet],
   template: `
-    <h1>Hello, World!</h1>
+    <main>
+      <header>
+        <img class="logo" src="assets/home-beach.png" alt="logo" aria-hidden="true" />
+      </header>
+        <section class="content">
+          <h1>Hello, World!</h1>
+        </section>
+    </main>
   `,
-  //templateUrl: './app.component.html',
+  styles: `
+    h1{
+      color: blue
+      }
+    img {
+      width: 25%;
+    }`,
+  //templateUrl: './app.component.html`
   //styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'beaches';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  //template:  `<img class="logo" src="beaches/src/assets/home-beach.png" alt="logo" aria-hidden="true" />`,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
